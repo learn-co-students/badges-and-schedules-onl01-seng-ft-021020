c2d829a17bcc9dfdfbc684b4de86245728ab151c
Victor Ibasco <victoribasco18@gmail.com>
2020-02-10 21:54:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  badge = "Hello, my name is #{name}."
+  badge
 end
 
 def batch_badge_creator(arr)
@@ -10,13 +11,23 @@ def batch_badge_creator(arr)
     badge = badge_maker(name)
     batch << badge
   end
+  batch
 end
 
 def assign_rooms(speakers)
   # rooms = (1..7).to_a
   room_assignments = []
   speakers.each.with_index do |speaker, idx|
-    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{idx + 1}"
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{idx + 1}!"
   end
   room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end 
+  assign_rooms(attendees).each do |room_assignment|
+    puts room_assignment
+  end
 end
