1dcb362e8f9f96ba8e07a2b8c93c779b633d367c
Dakota States <dakota.states@gmail.com>
2020-02-12 16:04:24 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each {|x| badges << badge_maker(x)}
+  badges
+end
+
+def assign_rooms(attendees)
+  names = []
+  attendees.each_with_index {|name,index| names << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  names
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{
+    |x| puts x
+  }
+  assign_rooms(attendees).each{
+    |x| puts x
+  }
+end
